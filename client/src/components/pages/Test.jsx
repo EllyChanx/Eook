9949cdc73c2377a880ef5cce895be7f0e01dd87f
elly <elly.chan.mda@gmail.com>
2019-02-28 00:24:24 +0000

    store fetched data to state, can show in ui

--- client/src/components/pages/Test.jsx
+++ client/src/components/pages/Test.jsx
@@ -9,26 +9,27 @@ class Test extends Component {
   }
 
   componentDidMount() {
-
+    let self = this;
     fetch('/search/index.xml?key='+ process.env.REACT_APP_GOODREAD_API_KEY +'&q=Ender%27s+Game')
       .then((response) => response.text())
         .then((responseText) => {
           parseString(responseText, function (err, result) {
-            console.log(result.GoodreadsResponse.search[0].results[0].work.map(
+            var data = result.GoodreadsResponse.search[0].results[0].work.map(
               work => ({
+                goodreadrating: work.average_rating[0],
+                publicationyear: work.original_publication_year[0]._,
                 goodreadsId: work.best_book[0].id[0]._,
                 title: work.best_book[0].title[0],
-                authors: work.best_book[0].author[0].name[0],
+                author: work.best_book[0].author[0].name[0],
                 covers: [work.best_book[0].image_url[0]]
               })
-            ))
+            );
+            self.setState({ results: data});
           });
         })
         .catch((err) => {
             console.log('Error fetching the feed: ', err)
     })
-
-
 
     // const url = "/book/review_counts.json?isbns=0441172717%2C0141439602&key=" + process.env.REACT_APP_GOODREAD_API_KEY; // site that doesn’t send Access-Control-*
     // return window.fetch(`${url}`)
@@ -38,13 +39,31 @@ class Test extends Component {
   }
 
   render() {
-
-    console.log(this.state.results)
-    return (
-      <div className="ui container">
-       <p> Tasdasdaest </p>
-      </div>
-    );
+    const results = this.state.results;
+        return (
+          <div>
+            {results.length
+              ?
+              results.map(book => {
+                return (
+                  <figure key={book.goodreadsId}>
+                    <img src={book.covers} alt='book'/>
+                    <div className="ribbon">
+                   <span>
+                    <div>{book.title}</div>
+                    <div>Authur: {book.author}</div>
+                  </span>
+                    </div>
+                    <figcaption>
+                      <div className="book-status"></div>
+                      <div>GoodRead Rating: {book.goodreadrating}</div>
+                    </figcaption>
+                  </figure>)
+              })
+              : <Container textAlign='center'>No Books found.</Container>
+            }
+          </div>
+        )
   }
 }
 
