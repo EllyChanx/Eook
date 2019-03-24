a2cf261f29c4df159ef627635de071663c6e3903
elly <elly.chan.mda@gmail.com>
2019-03-24 15:05:16 +0000

    adjust css for searchbar

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <TopNavigation />
         <Router history={createBrowserHistory()}>
           <Switch>
             <Route exact path="/" component={HomePage}/>
